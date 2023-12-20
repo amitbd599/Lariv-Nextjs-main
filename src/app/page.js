0a6fa7ba2ef9b740712b4875da1a84188863baf9
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-12-20 15:25:19 +0600

    udk

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,13 @@
-import { PrismaClient } from "@prisma/client";
 
-async function getData() {
-  const prisma = new PrismaClient();
-  let user = await prisma.user.findMany();
 
-  return { user };
-}
+
 export default async function Home() {
-  const data = await getData();
-  console.log(data.user);
+
 
   return (
     <main>
-      {/* <input type="text" ref={(input) => (emailRef = input)} />
-      <input type="password" ref={(input) => (passwordRef = input)} /> */}
-      {/* <button onClick={submit}>submit</button> */}
-      <br />
-      <br />
-      <br />
-      <div>
-        {data.user?.map((item, index) => (
-          <div key={index}>
-            <p>{item.email}</p>
-          </div>
-        ))}
-      </div>
+    
+   
     </main>
   );
 }
