ff079262f0a6319ab1825acbf60f6335a34500f9
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-12-20 14:01:29 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,25 @@
-"use client";
+import { PrismaClient } from "@prisma/client";
 
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
+async function getData() {
+  const prisma = new PrismaClient();
+  let user = await prisma.user.findMany();
 
-export default function Home() {
-  const [data, dataSet] = useState([]);
-  useEffect(() => {
-    async function fetchMyAPI() {
-      let response = await axios.get("/api/login");
+  return { user };
+}
+export default async function Home() {
+  const data = await getData();
+  console.log(data.user);
 
-      dataSet(response.data.data);
-    }
-
-    fetchMyAPI();
-  }, []);
-
-  let emailRef,
-    passwordRef = useRef();
-
-  let submit = () => {
-    let email = emailRef.value;
-    let password = passwordRef.value;
-
-    let res = axios.post("/api/login", { email, password });
-  };
   return (
     <main>
-      <input type="text" ref={(input) => (emailRef = input)} />
-      <input type="password" ref={(input) => (passwordRef = input)} />
-      <button onClick={submit}>submit</button>
+      {/* <input type="text" ref={(input) => (emailRef = input)} />
+      <input type="password" ref={(input) => (passwordRef = input)} /> */}
+      {/* <button onClick={submit}>submit</button> */}
       <br />
       <br />
       <br />
       <div>
-        {data.map((item, index) => (
+        {data.user?.map((item, index) => (
           <div key={index}>
             <p>{item.email}</p>
           </div>
