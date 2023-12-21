fc88ded9c3777df6579b78935e8bdfa10d12d225
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-12-21 13:29:37 +0600

    update data

--- src/app/dashboard/inbox/details/[slug]/page.js
+++ src/app/dashboard/inbox/details/[slug]/page.js
@@ -1,38 +1,37 @@
 'use client'
 import DashboardMasterLayout from '@/layout/DashboardMasterLayout';
-import { read_single_message__Request__API } from '@/utility/api';
+import client_api from '@/utility/api_fetch_fun';
 import React, { useEffect, useState } from 'react'
 
-const page = ({params}) => {
-    let id = params.slug;
-    const [data, setData] = useState([])
-    useEffect(() => {
+const page = ({ params }) => {
+  let id = params.slug;
+  const [data, setData] = useState([])
+  useEffect(() => {
 
-        read_single_message__Request__API(id).then((res)=>{
-        if(res?.status === true){
-
-          setData(res?.data[0]);
+    client_api.get(`/api/dashboard/message/read-single?id=${id}`).then((res) => {
+      if (res?.status === true) {
+        setData(res?.data[0]);
       }
-      })
-      }, []);
+    })
+  }, []);
 
   return (
     <main>
       <DashboardMasterLayout>
-      <div className="rounded-xl bg-[#36404A] p-[30px] m-[30px]">
-        <h2 className="text-white text-[20px]">Message from {data?.name}</h2>
-        <p className="text-[14px]">Email: {data?.email}</p>
-        <p className="text-[14px]">Website: {data?.website}</p>
-        <p className="text-[14px]">Send Date: {data?.createAt?.substring(0, 10)} </p>
-        <br/>
-        <div className="border p-[14px] rounded">
+        <div className="rounded-xl bg-[#36404A] p-[30px] m-[30px]">
+          <h2 className="text-white text-[20px]">Message from {data?.name}</h2>
+          <p className="text-[14px]">Email: {data?.email}</p>
+          <p className="text-[14px]">Website: {data?.website}</p>
+          <p className="text-[14px]">Send Date: {data?.createAt?.substring(0, 10)} </p>
+          <br />
+          <div className="border p-[14px] rounded">
             <p>
-                {data?.long_des}
+              {data?.long_des}
             </p>
+          </div>
+
         </div>
-        
-      </div>
-        
+
       </DashboardMasterLayout>
     </main>
   )
