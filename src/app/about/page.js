92647da8ea8d403a5e620a9a8ccf18a24ed40a66
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-12-21 10:26:39 +0600

    About page content edit section

---
+++ src/app/about/page.js
@@ -0,0 +1,24 @@
+import AboutComponent from "@/components/AboutComponent";
+import MasterLayout from "@/layout/MasterLayout";
+import { PrismaClient } from "@prisma/client";
+
+async function getData() {
+  const prisma = new PrismaClient();
+  let about_page_data = await prisma.about_page.findMany();
+  let experience_data = await prisma.experience.findMany();
+  let education_data = await prisma.education.findMany();
+  let skill_data = await prisma.skill.findMany();
+
+  return { about_page_data, experience_data, education_data, skill_data };
+}
+
+export default async function Page() {
+  const data = await getData();
+  return (
+    <main>
+      <MasterLayout>
+        <AboutComponent data={data} />
+      </MasterLayout>
+    </main>
+  );
+}
